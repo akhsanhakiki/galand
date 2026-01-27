e1f33e0b1857eaeecbd302b8bc7360cb31d24119
Muhammad Akhsan Hakiki <66757916+akhsanhakiki@users.noreply.github.com>
2026-01-27 11:50:18 +0700

    refactor: Switch from Netlify to Node adapter in Astro configuration for standalone mode and add start script in package.json for server entry point.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify";
 import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
@@ -8,7 +7,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
